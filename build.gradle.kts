4b32b1125923cf25f91c5b9ffa31a048a38f556c
Jakkawan <ada.devdroid@gmail.com>
2025-01-27 20:55:37 +0700

    v1.0.0

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    id("maven-publish")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("release") {
+            groupId = "com.github.AdaDevDroid" // GitHub Username
+            artifactId = "passcodeview" // ชื่อ Library
+            version = "1.0.0" // เวอร์ชันของ Library
+
+            artifact("libs/swipe-reveal-layout-1.4.1.aar") { // ระบุไฟล์ .aar ที่ต้องการเผยแพร่
+                extension = "aar" // ระบุชนิดไฟล์
+            }
+        }
+        repositories {
+            mavenLocal()
+        }
+    }
+}
